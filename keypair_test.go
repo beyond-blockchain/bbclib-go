693cf9040b856b4a14c22157f0b432b89a332e6d
Takeshi Kubo <takeshi@quvox.net>
2019-10-25 03:03:56 +0900

    implement key manipulation, sign/verify functions in Golang to remove libbbcsig dependency

--- keypair_test.go
+++ keypair_test.go
@@ -23,15 +23,13 @@ import (
 )
 
 func TestGenerateKeypair(t *testing.T) {
-	for curvetype := 1; curvetype < 3; curvetype++ {
-		t.Run("curvetype", func(t *testing.T) {
-			keypair := GenerateKeypair(curvetype, DefaultCompressionMode)
-			t.Logf("keypair: %v", keypair)
-			if len(keypair.Pubkey) != 65 {
-				t.Fatal("fail to generate keypair")
-			}
-		})
-	}
+	t.Run("simply generate keypair", func(t *testing.T) {
+		keypair, _ := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
+		t.Logf("keypair: %v", keypair)
+		if len(keypair.Pubkey) != 65 {
+			t.Fatal("fail to generate keypair")
+		}
+	})
 }
 
 func TestKeyPair_Sign_and_Verify(t *testing.T) {
@@ -40,103 +38,100 @@ func TestKeyPair_Sign_and_Verify(t *testing.T) {
 	t.Logf("SHA-256 digest : %x\n", digest)
 	t.Logf("SHA-256 digest2: %x\n", digest2)
 
-	for curvetype := 1; curvetype < 3; curvetype++ {
-		keypair := GenerateKeypair(curvetype, DefaultCompressionMode)
-		keypair2 := GenerateKeypair(curvetype, DefaultCompressionMode)
-		t.Run("curvetype", func(t *testing.T) {
-			t.Logf("Curvetype = %d", curvetype)
-			if len(keypair.Pubkey) != 65 {
-				t.Fatal("fail to generate keypair")
-			}
+	keypair, _ := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
+	keypair2, _ := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
+	t.Run("curvetype = KeyTypeEcdsaP256v1 only", func(t *testing.T) {
+		t.Logf("Curvetype = %d", KeyTypeEcdsaP256v1)
+		if len(keypair.Pubkey) != 65 {
+			t.Fatal("fail to generate keypair")
+		}
 
-			t.Logf("privkey   : %x\n", keypair.Privkey)
-			sig1 := keypair.Sign(digest[:])
-			t.Logf("signature : %x\n", sig1)
-			if len(sig1) != 64 {
-				t.Fatal("fail to sign")
-			}
-			result := keypair.Verify(digest[:], sig1)
-			if !result {
-				t.Fatal("fail to verify")
-			}
-			t.Log("[sig1] Verify succeeded")
-			result = keypair.Verify(digest2[:], sig1)
-			if result {
-				t.Fatal("[invalid digest] Verify returns true but not correct...")
-			}
-			t.Log("[invalid digest] Verify failed as expected")
+		t.Logf("privkey   : %x\n", keypair.Privkey)
+		sig1 := keypair.Sign(digest[:])
+		t.Logf("signature : %x (%d)\n", sig1, len(sig1))
+		if len(sig1) != 64 {
+			t.Fatal("fail to sign")
+		}
+		result := keypair.Verify(digest[:], sig1)
+		if !result {
+			t.Fatal("fail to verify")
+		}
+		t.Log("[sig1] Verify succeeded")
+		result = keypair.Verify(digest2[:], sig1)
+		if result {
+			t.Fatal("[invalid digest] Verify returns true but not correct...")
+		}
+		t.Log("[invalid digest] Verify failed as expected")
 
-			t.Logf("privkey2  : %x\n", keypair2.Privkey)
-			sig2 := keypair2.Sign(digest[:])
-			t.Logf("signature2: %x\n", sig2)
-			if len(sig2) != 64 {
-				t.Fatal("fail to sign")
-			}
-			result = keypair2.Verify(digest[:], sig2)
-			if !result {
-				t.Fatal("fail to verify")
-			}
-			t.Log("[sig2] Verify succeeded")
-			result = keypair2.Verify(digest2[:], sig2)
-			if result {
-				t.Fatal("[invalid digest] Verify returns true but not correct...")
-			}
-			t.Log("[invalid digest] Verify failed as expected")
+		t.Logf("privkey2  : %x\n", keypair2.Privkey)
+		sig2 := keypair2.Sign(digest[:])
+		t.Logf("signature2: %x (%d)\n", sig2, len(sig2))
+		if len(sig2) != 64 {
+			t.Fatal("fail to sign")
+		}
+		result = keypair2.Verify(digest[:], sig2)
+		if !result {
+			t.Fatal("fail to verify")
+		}
+		t.Log("[sig2] Verify succeeded")
+		result = keypair2.Verify(digest2[:], sig2)
+		if result {
+			t.Fatal("[invalid digest] Verify returns true but not correct...")
+		}
+		t.Log("[invalid digest] Verify failed as expected")
 
-			result = keypair2.Verify(digest[:], sig1)
-			if result {
-				t.Fatal("[swap] Verify returns true but not correct...")
-			}
-			t.Log("[swap] Verify failed as expected")
-			result = keypair.Verify(digest[:], sig2)
-			if result {
-				t.Fatal("[swap] Verify returns true but not correct...")
-			}
-			t.Log("[swap] Verify failed as expected")
-		})
-	}
+		result = keypair2.Verify(digest[:], sig1)
+		if result {
+			t.Fatal("[swap] Verify returns true but not correct...")
+		}
+		t.Log("[swap] Verify failed as expected")
+		result = keypair.Verify(digest[:], sig2)
+		if result {
+			t.Fatal("[swap] Verify returns true but not correct...")
+		}
+		t.Log("[swap] Verify failed as expected")
+	})
 }
 
 func TestVerifyBBcSignature(t *testing.T) {
 	digest := sha256.Sum256([]byte("aaaaaaaaaaa"))
 
-	for curvetype := 1; curvetype < 3; curvetype++ {
-		t.Run("curvetype", func(t *testing.T) {
-			keypair := GenerateKeypair(curvetype, DefaultCompressionMode)
-			sig := BBcSignature{}
-			sig.SetPublicKey(uint32(curvetype), &keypair.Pubkey)
-			signature := keypair.Sign(digest[:])
-			sig.SetSignature(&signature)
-			result1 := keypair.Verify(digest[:], sig.Signature)
-			if !result1 {
-				t.Fatal("fail to verify")
-			}
-			t.Log("Verify succeeded")
+	t.Run("curvetype", func(t *testing.T) {
+		keypair, _ := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
+		sig := BBcSignature{}
+		sig.SetPublicKey(uint32(KeyTypeEcdsaP256v1), &keypair.Pubkey)
+		signature := keypair.Sign(digest[:])
+		sig.SetSignature(&signature)
+		result1 := keypair.Verify(digest[:], sig.Signature)
+		if !result1 {
+			t.Fatal("fail to verify")
+		}
+		t.Log("Verify succeeded")
 
-			result := VerifyBBcSignature(digest[:], &sig)
-			if !result {
-				t.Fatal("fail to verify")
-			}
-			t.Log("Verify succeeded")
+		result := VerifyBBcSignature(digest[:], &sig)
+		if !result {
+			t.Fatal("fail to verify")
+		}
+		t.Log("Verify succeeded")
 
-			keypair2 := GenerateKeypair(curvetype, DefaultCompressionMode)
-			sig2 := BBcSignature{}
-			sig2.SetPublicKey(uint32(curvetype), &keypair.Pubkey)
-			signature2 := keypair2.Sign(digest[:])
-			sig2.SetSignature(&signature2)
-			result = sig2.Verify(digest[:])
-			if result {
-				t.Fatal("Verify returns true but not correct...")
-			}
-			t.Log("Verify failed as expected")
-		})
-	}
+		keypair2, _ := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
+		sig2 := BBcSignature{}
+		sig2.SetPublicKey(uint32(KeyTypeEcdsaP256v1), &keypair.Pubkey)
+		signature2 := keypair2.Sign(digest[:])
+		sig2.SetSignature(&signature2)
+		result = sig2.Verify(digest[:])
+		if result {
+			t.Fatal("Verify returns true but not correct...")
+		}
+		t.Log("Verify failed as expected")
+	})
 }
 
 func TestKeyPair_ConvertFromPem(t *testing.T) {
-	pem := "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIIMVMPKLJqivgRDpRDaWJCOnob6s/+t4MdoFN/8PVkNSoAcGBSuBBAAK\noUQDQgAE/k1ZM/Ker1+N0+Lg5za0sJZeSAAeYwDEWnkgnkCynErs74G/tAnu/lcu\nk8kzAivYm8mitIpJJw1OdjCDJI457g==\n-----END EC PRIVATE KEY-----"
-	keypair := KeyPair{CurveType: KeyTypeEcdsaSECP256k1}
-	keypair.ConvertFromPem(pem, DefaultCompressionMode)
+	pem := "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIJ6T6fh4uQCmxImVaRueeTuZ4rSvcJtO1mRQgolFs4fgoAoGCCqGSM49\nAwEHoUQDQgAEDbE3poddh6YhzVAkOO6edq9VimbnD5t46eu/9CW6Y2C3uQxaBV39\nJFLML26BlpFqRD/W+GgqpTgTfbWq0cJj+A==\n-----END EC PRIVATE KEY-----"
+	keypair := KeyPair{CurveType: KeyTypeEcdsaP256v1}
+	err := keypair.ConvertFromPem(pem, DefaultCompressionMode)
+	t.Logf("error: %v", err)
 	t.Logf("keypair: %v", keypair)
 
 	if len(keypair.Privkey) != 32 {
@@ -146,12 +141,12 @@ func TestKeyPair_ConvertFromPem(t *testing.T) {
 }
 
 func TestKeyPair_Export_Import(t *testing.T) {
-	keypair := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
+	keypair, _ := GenerateKeypair(KeyTypeEcdsaP256v1, DefaultCompressionMode)
 	der := keypair.OutputDer()
-	pem := keypair.OutputPem()
+	pem, _ := keypair.OutputPem()
 
-	keypair2 := KeyPair{CurveType: 0}
-	keypair2.ConvertFromDer(der, DefaultCompressionMode)
+	keypair2 := KeyPair{CurveType: KeyTypeNotInitialized}
+	_ = keypair2.ConvertFromDer(der, DefaultCompressionMode)
 	if bytes.Compare(keypair.Privkey, keypair2.Privkey) != 0 {
 		t.Fatal("export or import is failed (DER)")
 	}
@@ -162,8 +157,8 @@ func TestKeyPair_Export_Import(t *testing.T) {
 	pubkey2 := keypair2.GetPublicKeyCompressed()
 	t.Logf("public key (compressed): %v", pubkey2)
 
-	keypair3 := KeyPair{CurveType: 0}
-	keypair3.ConvertFromPem(pem, DefaultCompressionMode)
+	keypair3 := KeyPair{CurveType: KeyTypeNotInitialized}
+	_ = keypair3.ConvertFromPem(pem, DefaultCompressionMode)
 	if bytes.Compare(keypair.Privkey, keypair3.Privkey) != 0 {
 		t.Fatal("export or import is failed (PEM)")
 	}
